97a94baad4fe26ccf15def0c746013b23f03c8fc
raffaelecapaldo <raffaelecpl@gmail.com>
2023-04-11 14:07:18 +0200

    Added getMail

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue
+
+
+const app = createApp({
+  data() {
+    return {
+      titolo: "Contatti mail",
+      basePath: "https://flynn.boolean.careers/exercises/api/",
+      emails: [],
+      numMail: 10
+    }
+  },
+  methods: {
+    getMail() {
+      for (i = 0; i < this.numMail; i++) {
+        axios.get(this.basePath + "random/mail").then((response) => {
+          email = response.data.response;
+          this.emails.push(email);
+        }
+
+        )
+
+      }
+    }
+
+  },
+  mounted() {
+    this.getMail();
+  }
+})
+
+app.mount('#app')
