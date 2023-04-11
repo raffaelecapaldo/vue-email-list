14498806bdd99a379769edbd9bf9b5c3d0f1ccb2
raffaelecapaldo <raffaelecpl@gmail.com>
2023-04-11 15:02:09 +0200

    Added loading splash

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
-const { createApp } = Vue
+const {
+	createApp
+} = Vue
 
 
 const app = createApp({
-  data() {
-    return {
-      titolo: "Contatti mail",
-      basePath: "https://flynn.boolean.careers/exercises/api/",
-      emails: [],
-      numMail: 10
-    }
-  },
-  methods: {
-    getMail() {
-      for (i = 0; i < this.numMail; i++) {
-        axios.get(this.basePath + "random/mail").then((response) => {
-          email = response.data.response;
-          this.emails.push(email);
-        }
+	data() {
+		return {
+			titolo: "Contatti mail",
+			basePath: "https://flynn.boolean.careers/exercises/api/",
+			emails: [],
+			numMail: 10
+		}
+	},
+	methods: {
+		getMail() {
+			for (i = 0; i < this.numMail; i++) {
+				axios.get(this.basePath + "random/mail").then((response) => {
+						email = response.data.response;
+						console.log(email);
+						this.emails.push(email);
+					}
 
-        )
+				)
 
-      }
-    }
+			}
+		}
 
-  },
-  mounted() {
-    this.getMail();
-  }
+	},
+	mounted() {
+		this.getMail();
+	}
 })
 
 app.mount('#app')
